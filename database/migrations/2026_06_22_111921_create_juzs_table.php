589fcac3fff17c113e3e58be3d6c6c0ed3d2f5b1
Bilal <bilalkhan2107007@gmail.com>
2026-06-22 16:36:14 +0500

    Updated code

--- database/migrations/2026_06_22_111921_create_juzs_table.php
+++ database/migrations/2026_06_22_111921_create_juzs_table.php
@@ -10,12 +10,14 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('juzs', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('juzs', function (Blueprint $table) {
+        $table->id();
+        $table->integer('juz_number')->unique(); 
+        $table->json('data'); 
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
